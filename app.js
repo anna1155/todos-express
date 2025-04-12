b61127b88c96431dd1404c4a62bd693ca55ca0fd
anna1155 <eyluleylul159@gmail.com>
2025-04-12 21:11:50 +0200

    Update: describe what you changed

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const {readFile, writeFile} = require('./helper/file.helper');
 const filePath = './data/todos.json';
 
 const app = express();
+app.use(cors());
 const port = 5000;
 
 app.use(express.urlencoded({extended: true}));

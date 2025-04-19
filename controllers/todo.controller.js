c431565b55ec1b0cbf3fb522d46a33be952a8b49
anna1155 <eyluleylul159@gmail.com>
2025-04-19 14:22:10 +0200

    Linked with prisma

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -5,18 +5,18 @@ const getTodoById = async (req,res) => {
     const todo = await TodoDBModel.findOne(id);
     if(!todo) {
         return res.status(500).json({
-            message: 'Task not found'
+            message: '***Task not found***'
         });
     }
     return res.status(200).json({
-        message: 'Todo fetched successfully***',
+        message: '***Todo fetched successfully***',
         data: todo
     });
 };
 
 const getTodos = async (req,res) => {
     return res.status(200).json({
-        message: 'Fetched all todos successfully***',
+        message: '***Fetched all todos successfully***',
         data: await TodoDBModel.getAll()
     });
 };
@@ -24,14 +24,14 @@ const getTodos = async (req,res) => {
 const createTodo = async (req,res) => {
     const title = req.body.title;
     if(!title) return res.status(400).json({
-        message: 'Title not found***',
+        message: '***Title not found***',
     });
     await TodoDBModel.createOne({
         title: title
     });
 
     return res.status(201).json({
-        message: 'Task created successfully***',
+        message: '***Task created successfully***',
     }) 
 };
 
@@ -39,18 +39,18 @@ const updateTodo = async (req,res, next) => {
     const id = req.params.id;
     const todo = await TodoDBModel.findOne(id);
     if(!todo) return res.status(404).json({
-        message: 'Task not found***'
+        message: '***Task not found***'
     })
 
     const {title, isCompleted} = req.body;
     const data = {title, isCompleted};
     if(title && isCompleted) await TodoDBModel.updateOne(id, data);
     if(!isCompleted && !title) return res.status(404).json({
-        message: 'Task not found***'
+        message: '***Task not found***'
     });
 
     return res.status(200).json({
-        message: 'Task updated successfully***',
+        message: '***Task updated successfully***',
         data: await TodoDBModel.findOne(id)
     })
 };
@@ -60,13 +60,13 @@ const deleteTodo =  async (req,res) => {
     const todo = await TodoDBModel.findOne(id);
 
     if(todo < 0) return res.status(404).json({
-        message: 'Task not found***'
+        message: '***Task not found***'
     })
 
     await TodoDBModel.deleteOne(todo);
 
     return res.status(200).json({
-        message:'Task deleted successfully***'
+        message:'***Task deleted successfully***'
     })
 };
 
